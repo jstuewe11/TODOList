ce97f8101a72c3779ec9b30973416a1d76d5369a
jstuewe11 <94012396+jstuewe11@users.noreply.github.com>
2023-09-15 11:07:24 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Simple project to learn values in typescript
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'FirstProject';
+}
